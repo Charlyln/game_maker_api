5002f4956bb01d25c01db4cc8e62c65a5d6a3404
Charles Henry Le Nué <charles-henry.lenue@onrewind.com>
2023-01-04 01:42:23 +0100

    resolve port

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ async function main() {
     res.status(200).send('GAME MAKER API');
   });
 
-  httpServer.listen(8080, () => {
-    console.log(`App listening at port:${8080}`);
+  httpServer.listen(process.env.PORT || 8080, () => {
+    console.log(`App listening at port:${process.env.PORT || 8080}`);
   });
 }
 
