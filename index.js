1e81e862cd5ec211b38df932cd42c0a3985c25de
Charles Henry Le Nué <charles-henry.lenue@onrewind.com>
2023-01-04 13:34:54 +0100

    feat/ can update user score and sort scores for display

--- index.js
+++ index.js
@@ -30,14 +30,15 @@ async function main() {
   //   await sequelize.connect();
   // })();
 
-  const scores = [{ name: 'charly', score: 100 }];
+  const scores = [{ name: 'charly', score: 0 }];
 
   app.get('/version', (req, res) => {
     res.status(200).json({ version: '0.0.1' });
   });
 
   app.get('/scores', (req, res) => {
-    res.status(200).json({ scores });
+    const sorted = scores.sort((a, b) => b.score - a.score);
+    res.status(200).json({ scores: sorted });
   });
 
   app.post('/name/:name/score/:score', (req, res) => {
@@ -45,14 +46,25 @@ async function main() {
       console.log(req.params.name);
       console.log(req.params.score);
 
-      const newscore = {
-        name: req.params.name,
-        score: parseInt(req.params.score),
-      };
+      const index = scores.findIndex(
+        (obj) => obj.name.toLowerCase() === req.params.name.toLowerCase()
+      );
 
-      scores.push(newscore);
+      if (index !== -1) {
+        if (parseInt(req.params.score) > scores[index].score) {
+          scores[index].score = parseInt(req.params.score);
+        }
 
-      res.status(200).json(newscore);
+        res.status(200).json(scores[index]);
+      } else {
+        const newscore = {
+          name: req.params.name,
+          score: parseInt(req.params.score),
+        };
+
+        scores.push(newscore);
+        res.status(200).json(newscore);
+      }
     } catch (error) {
       res.status(400).json({ error });
     }
